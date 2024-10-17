222ae88bb7cf86c542c6c31f44effa2875fab31a
EliaderMalaquias <joseeliader190@gmail.com>
2024-10-17 15:28:54 -0300

    Criando Entidades #1

---
+++ src/main/java/com/UnitTasks/ToDoList/model/User.java
@@ -0,0 +1,29 @@
+package com.UnitTasks.ToDoList.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    @Temporal(TemporalType.DATE)
+    private Date createdIn;
+}
