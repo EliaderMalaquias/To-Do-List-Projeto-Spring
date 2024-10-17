f28dc9d1ff86849875b734f50c0da5c75fddd0b0
EliaderMalaquias <joseeliader190@gmail.com>
2024-10-17 15:56:20 -0300

    Adicionando Funcionalidade nos Controllers

--- src/main/java/com/UnitTasks/ToDoList/controller/UserController.java
+++ src/main/java/com/UnitTasks/ToDoList/controller/UserController.java
@@ -4,10 +4,9 @@ import com.UnitTasks.ToDoList.dtos.request.UserRequestDTO;
 import com.UnitTasks.ToDoList.dtos.response.UserResponseDTO;
 import com.UnitTasks.ToDoList.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -19,5 +18,20 @@ public class UserController {
     @PostMapping
     public UserResponseDTO createUser(@RequestBody UserRequestDTO request) {
         return service.createUser(request);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> getAllUsers() {
+        return service.getAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public UserResponseDTO getUserById(@PathVariable(value = "id") Long Id) {
+        return service.getById(Id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteUser(@PathVariable(value = "id") Long Id) {
+        service.delete(Id);
     }
 }
